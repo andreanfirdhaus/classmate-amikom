86e79b4d0664bb8226f74006a079d2c3f6928cd7
andreanfirdhaus <andreanfirdhaus22@gmail.com>
2023-11-01 14:24:38 +0700

    change from text input to select input on top 3 froms

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -60,19 +60,28 @@ function Modal({ updatePathURL }) {
                                             Tahun Angkatan
                                         </span>
                                     </label>
-                                    <input
-                                        type="text"
-                                        placeholder="20xx"
-                                        className={`input input-bordered input-sm w-full max-w-xs placeholder:tracking-wider placeholder:text-gray-400 placeholder:text-xs ${
+                                    <select
+                                        className={`select select-bordered select-sm w-full max-w-xs ${
                                             error && !yearCode
                                                 ? "border-2 border-error"
                                                 : errorBorder
                                         }`}
                                         value={yearCode}
+                                        defaultValue=""
                                         onChange={(e) =>
                                             setYearCode(e.target.value)
                                         }
-                                    />
+                                    >
+                                        <option hidden value="">
+                                            20xx
+                                        </option>
+                                        <option value="2018">2018</option>
+                                        <option value="2019">2019</option>
+                                        <option value="2020">2020</option>
+                                        <option value="2021">2021</option>
+                                        <option value="2022">2022</option>
+                                        <option value="2023">2023</option>
+                                    </select>
                                 </div>
                             </div>
                             <div className="col-span-2 row-start-2">
@@ -83,19 +92,28 @@ function Modal({ updatePathURL }) {
                                             Kode Angkatan
                                         </span>
                                     </label>
-                                    <input
-                                        type="text"
-                                        placeholder="xx"
-                                        className={`input input-bordered input-sm w-full max-w-xs placeholder:tracking-wider placeholder:text-gray-400 placeholder:text-xs ${
+                                    <select
+                                        className={`select select-bordered select-sm w-full max-w-xs ${
                                             error && !classOfCode
                                                 ? "border-2 border-error"
                                                 : errorBorder
                                         }`}
                                         value={classOfCode}
+                                        defaultValue=""
                                         onChange={(e) =>
                                             setClassOfCode(e.target.value)
                                         }
-                                    />
+                                    >
+                                        <option hidden value="">
+                                            xx
+                                        </option>
+                                        <option value="18">18</option>
+                                        <option value="19">19</option>
+                                        <option value="20">20</option>
+                                        <option value="21">21</option>
+                                        <option value="22">22</option>
+                                        <option value="23">23</option>
+                                    </select>
                                 </div>
                             </div>
                             <div className="col-span-2 col-start-3 row-start-2">
@@ -106,19 +124,27 @@ function Modal({ updatePathURL }) {
                                             Kode Prodi
                                         </span>
                                     </label>
-                                    <input
-                                        type="text"
-                                        placeholder="xx"
-                                        className={`input input-bordered input-sm w-full max-w-xs placeholder:tracking-wider placeholder:text-gray-400 placeholder:text-xs ${
+                                    <select
+                                        className={`select select-bordered select-sm w-full max-w-xs ${
                                             error && !majorCode
                                                 ? "border-2 border-error"
                                                 : errorBorder
                                         }`}
                                         value={majorCode}
+                                        defaultValue=""
                                         onChange={(e) =>
                                             setMajorCode(e.target.value)
                                         }
-                                    />
+                                    >
+                                        <option hidden value="">
+                                            xx
+                                        </option>
+                                        <option value="11">11</option> {/* 1 */}
+                                        <option value="12">12</option> {/* 3 */}
+                                        <option value="95">95</option> {/* 5 */}
+                                        <option value="67">67</option> {/* 4 */}
+                                        <option value="96">96</option> {/* 2 */}
+                                    </select>
                                 </div>
                             </div>
                             <div className="col-span-2 row-start-3">
