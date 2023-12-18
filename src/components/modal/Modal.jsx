8c3087f327146c1991957666947f21eeae8a3e4a
andreanfirdhaus <andreanfirdhaus22@gmail.com>
2023-12-18 20:48:55 +0700

    improve state and validate form

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,213 +1,175 @@
-import React, { useState } from "react";
+import React from "react";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 import { IoClose } from "react-icons/io5";
 
-function Modal({ updatePathURL }) {
-    const [yearCode, setYearCode] = useState("");
-    const [classOfCode, setClassOfCode] = useState("");
-    const [majorCode, setMajorCode] = useState("");
-    const [startNIM, setStartNIM] = useState("");
-    const [endNIM, setEndNIM] = useState("");
+const schema = yup
+    .object()
+    .shape({
+        tahun_angkatan: yup.string().required("tahun angkatan is required"),
+        kode_prodi: yup.string().required("kode prodi is required"),
+        nim_awal: yup.string().required("nim awal is required"),
+        nim_akhir: yup.string().required("nim akhir is required"),
+    })
+    .required();
 
-    const [error, setError] = useState("");
+function Modal({ updatePathURL }) {
+    const {
+        register,
+        handleSubmit,
+        reset,
+        formState: { errors },
+    } = useForm({ resolver: yupResolver(schema) });
 
     const closeModal = () => {
-        // Reset form when close
-        setYearCode("");
-        setClassOfCode("");
-        setMajorCode("");
-        setStartNIM("");
-        setEndNIM("");
-        setError(""); // Reset error messages
-
         const modal = document.getElementById("my_modal_3");
         if (modal) {
             modal.close(); // Close modal
         }
+        reset(); // Reset data
     };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        if (yearCode && classOfCode && majorCode && startNIM && endNIM) {
-            // Only if all inputs are filled, call the updatePathURL function
-            updatePathURL(yearCode, classOfCode, majorCode, startNIM, endNIM);
-            closeModal(); // Close modal after submit
-        } else {
-            setError("Please complete the form"); // Error messages
-        }
+    const onSubmit = (data) => {
+        updatePathURL(
+            data.tahun_angkatan,
+            data.kode_prodi,
+            data.nim_awal,
+            data.nim_akhir
+        );
+        closeModal();
     };
-
-    const errorBorder =
-        "border" + (error ? "border-2 border-gray-300" : "border-error");
 
     return (
         <>
             <dialog id="my_modal_3" className="modal">
                 <div className="modal-box">
-                    <form method="dialog">
-                        {/* if there is a button in form, it will close the modal */}
-                        <button
-                            className="btn btn-sm btn-circle btn-ghost absolute right-3 top-3"
-                            onClick={closeModal}
-                        >
-                            <IoClose size={25} />
-                        </button>
-                        <div className="grid grid-cols-4 grid-rows-4 gap-5">
-                            <div className="col-span-2">
-                                {/* form input 1 */}
-                                <div className="form-control w-full max-w-xs">
-                                    <label className="label">
-                                        <span className="label-text">
-                                            Tahun Angkatan
-                                        </span>
-                                    </label>
-                                    <select
-                                        className={`select select-bordered select-sm w-full max-w-xs ${
-                                            error && !yearCode
-                                                ? "border-2 border-error"
-                                                : errorBorder
-                                        }`}
-                                        value={yearCode}
-                                        defaultValue=""
-                                        onChange={(e) =>
-                                            setYearCode(e.target.value)
-                                        }
-                                    >
-                                        <option hidden value="">
-                                            20xx
-                                        </option>
-                                        <option value="2018">2018</option>
-                                        <option value="2019">2019</option>
-                                        <option value="2020">2020</option>
-                                        <option value="2021">2021</option>
-                                        <option value="2022">2022</option>
-                                        <option value="2023">2023</option>
-                                    </select>
-                                </div>
-                            </div>
-                            <div className="col-span-2 row-start-2">
-                                {/* form input 2 */}
-                                <div className="form-control w-full max-w-xs">
-                                    <label className="label">
-                                        <span className="label-text">
-                                            Kode Angkatan
-                                        </span>
-                                    </label>
-                                    <select
-                                        className={`select select-bordered select-sm w-full max-w-xs ${
-                                            error && !classOfCode
-                                                ? "border-2 border-error"
-                                                : errorBorder
-                                        }`}
-                                        value={classOfCode}
-                                        defaultValue=""
-                                        onChange={(e) =>
-                                            setClassOfCode(e.target.value)
-                                        }
-                                    >
-                                        <option hidden value="">
-                                            xx
-                                        </option>
-                                        <option value="18">18</option>
-                                        <option value="19">19</option>
-                                        <option value="20">20</option>
-                                        <option value="21">21</option>
-                                        <option value="22">22</option>
-                                        <option value="23">23</option>
-                                    </select>
-                                </div>
-                            </div>
-                            <div className="col-span-2 col-start-3 row-start-2">
-                                {/* form input 3 */}
-                                <div className="form-control w-full max-w-xs">
-                                    <label className="label">
-                                        <span className="label-text">
-                                            Kode Prodi
-                                        </span>
-                                    </label>
-                                    <select
-                                        className={`select select-bordered select-sm w-full max-w-xs ${
-                                            error && !majorCode
-                                                ? "border-2 border-error"
-                                                : errorBorder
-                                        }`}
-                                        value={majorCode}
-                                        defaultValue=""
-                                        onChange={(e) =>
-                                            setMajorCode(e.target.value)
-                                        }
-                                    >
-                                        <option hidden value="">
-                                            xx
-                                        </option>
-                                        <option value="11">11</option> {/* 1 */}
-                                        <option value="12">12</option> {/* 3 */}
-                                        <option value="95">95</option> {/* 5 */}
-                                        <option value="67">67</option> {/* 4 */}
-                                        <option value="96">96</option> {/* 2 */}
-                                    </select>
-                                </div>
-                            </div>
-                            <div className="col-span-2 row-start-3">
-                                {/* form input 4 */}
-                                <div className="form-control w-full max-w-xs">
-                                    <label className="label">
-                                        <span className="label-text">
-                                            Start NIM
-                                        </span>
-                                    </label>
-                                    <input
-                                        type="text"
-                                        placeholder="4 digits"
-                                        className={`input input-bordered input-sm w-full max-w-xs placeholder:tracking-wider placeholder:text-gray-400 placeholder:text-xs ${
-                                            error && !startNIM
-                                                ? "border-2 border-error"
-                                                : errorBorder
-                                        }`}
-                                        value={startNIM}
-                                        onChange={(e) =>
-                                            setStartNIM(e.target.value)
-                                        }
-                                    />
-                                </div>
-                            </div>
-                            <div className="col-span-2 col-start-3 row-start-3">
-                                {/* form input 5 */}
-                                <div className="form-control w-full max-w-xs">
-                                    <label className="label">
-                                        <span className="label-text font-normal">
-                                            End NIM
-                                        </span>
-                                    </label>
-                                    <input
-                                        type="text"
-                                        placeholder="4 digits"
-                                        className={`input input-bordered input-sm w-full max-w-xs placeholder:tracking-wider placeholder:text-gray-400 placeholder:text-xs ${
-                                            error && !endNIM
-                                                ? "border-2 border-error"
-                                                : errorBorder
-                                        }`}
-                                        value={endNIM}
-                                        onChange={(e) =>
-                                            setEndNIM(e.target.value)
-                                        }
-                                    />
-                                </div>
-                            </div>
-                            <div className="col-span-4 row-start-4">
-                                {error && (
-                                    <div className="tracking-wide text-error label-text font-medium text-center py-2">
-                                        {error}
-                                    </div>
-                                )}
-                                {/* button */}
-                                <button
-                                    className="btn btn-sm btn-block btn-neutral transition"
-                                    type="submit"
-                                    onClick={handleSubmit}
+                    <button
+                        className="btn btn-sm btn-circle btn-ghost absolute right-3 top-3"
+                        onClick={closeModal}
+                    >
+                        <IoClose size={25} />
+                    </button>
+                    <form method="dialog" onSubmit={handleSubmit(onSubmit)}>
+                        <div className="space-y-3 rounded-md py-2">
+                            <div className="form-control w-full">
+                                <label className="label">
+                                    <span className="label-text md:text-base font-medium">
+                                        Tahun Angkatan
+                                    </span>
+                                </label>
+                                <select
+                                    className={`select select-bordered select-sm w-full ${
+                                        errors.tahun_angkatan?.message
+                                            ? "border-2 border-error"
+                                            : ""
+                                    }`}
+                                    {...register("tahun_angkatan", {
+                                        required: true,
+                                    })}
                                 >
-                                    Check
-                                </button>
+                                    <option value="" disabled selected>
+                                        Select...
+                                    </option>
+                                    <option value="2018">2018</option>
+                                    <option value="2019">2019</option>
+                                    <option value="2020">2020</option>
+                                    <option value="2021">2021</option>
+                                    <option value="2022">2022</option>
+                                    <option value="2023">2023</option>
+                                </select>
                             </div>
+
+                            <div className="form-control w-full">
+                                <label className="label">
+                                    <span className="label-text md:text-base font-medium">
+                                        Prodi
+                                    </span>
+                                </label>
+                                <select
+                                    className={`select select-bordered select-sm w-full ${
+                                        errors.kode_prodi?.message
+                                            ? "border-2 border-error"
+                                            : ""
+                                    }`}
+                                    {...register("kode_prodi")}
+                                >
+                                    <option value="" disabled selected>
+                                        Select...
+                                    </option>
+                                    <option value="11">Informatika</option>
+                                    <option value="12">Sistem Informasi</option>
+                                    <option value="95">
+                                        Hubungan Internasional
+                                    </option>
+                                    <option value="67">
+                                        Ilmu Komunikasi Internasional
+                                    </option>
+                                    <option value="82">
+                                        Teknologi Informasi
+                                    </option>
+                                    <option value="96">Ilmu Komunikasi</option>
+                                </select>
+                            </div>
+
+                            <div className="form-control w-full">
+                                <label className="label">
+                                    <span className="label-text md:text-base font-medium">
+                                        NIM Awal
+                                    </span>
+                                </label>
+                                <input
+                                    type="number"
+                                    placeholder="4 digits"
+                                    className={`input input-bordered input-sm w-full placeholder:tracking-wider placeholder:text-gray-400 placeholder:text-xs ${
+                                        errors.nim_awal?.message
+                                            ? " border-2 border-error"
+                                            : ""
+                                    }`}
+                                    {...register("nim_awal")}
+                                />
+                            </div>
+
+                            <div className="form-control w-full">
+                                <label className="label">
+                                    <span className="label-text md:text-base font-medium">
+                                        NIM Akhir
+                                    </span>
+                                </label>
+                                <input
+                                    type="number"
+                                    placeholder="4 digits"
+                                    className={`input input-bordered input-sm w-full placeholder:tracking-wider placeholder:text-gray-400 placeholder:text-xs ${
+                                        errors.nim_akhir?.message
+                                            ? " border-2 border-error"
+                                            : ""
+                                    }`}
+                                    {...register("nim_akhir")}
+                                />
+                            </div>
+                        </div>
+
+                        <div className="pt-10 relative">
+                            {Object.keys(errors).length > 1 ? (
+                                <div className="absolute z-10 top-1 left-1/2 transform -translate-x-1/2 tracking-wide text-error label-text font-medium text-center capitalize w-full">
+                                    Lengkapi semua field diatas
+                                </div>
+                            ) : Object.keys(errors).length < 2 ? (
+                                <div className="absolute z-10 top-1 left-1/2 transform -translate-x-1/2 tracking-wide text-error label-text font-medium text-center capitalize w-full">
+                                    {errors.tahun_angkatan?.message ||
+                                        errors.kode_prodi?.message ||
+                                        errors.nim_awal?.message ||
+                                        errors.nim_akhir?.message}
+                                </div>
+                            ) : (
+                                ""
+                            )}
+                            <button
+                                className="btn btn-sm btn-block btn-neutral transition"
+                                type="submit"
+                            >
+                                Check
+                            </button>
                         </div>
                     </form>
                 </div>
