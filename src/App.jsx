e697a006033225369e63348f260b004dc84310f4
andreanfirdhaus <andreanfirdhaus22@gmail.com>
2024-05-08 21:35:00 +0700

    fuck code, push on repo

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,19 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Classmates from "./pages/Classmates";
 import Graduated from "./pages/Graduated";
-import Footer from "./components/Footer";
+import { ThemeProvider } from "@/components/theme-provider";
 import Navbar from "./components/Navbar";
 
 function App() {
     return (
         <>
-            <Navbar title="Amikom" />
-            <Routes>
-                <Route path="/" element={<Classmates />} />
-                <Route path="graduated" element={<Graduated />} />
-            </Routes>
-            <Footer />
+            <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={<Classmates />} />
+                    <Route path="graduated" element={<Graduated />} />
+                </Routes>
+            </ThemeProvider>
         </>
     );
 }
