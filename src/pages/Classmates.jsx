2c55118ad3f90043672d71897ac91878344735e1
andreanfirdhaus <andreanfirdhaus22@gmail.com>
2024-05-11 00:52:23 +0700

    feat: added lottie animation

--- src/pages/Classmates.jsx
+++ src/pages/Classmates.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect, useContext } from "react";
 import { DataContext } from "../components/data-context";
 import getUrl from "../config/getUrl";
 import Content from "../components/layout/content";
+import LottiePlayer from "../components/layout/lottie-player";
 
 export default function Classmates() {
   const { data } = useContext(DataContext);
@@ -27,6 +28,10 @@ export default function Classmates() {
 
   return (
     <main className="container mx-auto w-full px-6 sm:px-0 md:w-[48.5rem] mt-20">
+      <LottiePlayer
+        src={"https://assets5.lottiefiles.com/packages/lf20_rc6CDU.json"}
+        width="260px"
+      />
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-6 content-center">
         <Content data={url} />
       </div>
