2a369d17adb5b635a8c3052540804577681a2995
andreanfirdhaus <andreanfirdhaus22@gmail.com>
2024-05-10 09:27:03 +0700

    fix: add useContext to access global data

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,22 @@ import { Routes, Route } from "react-router-dom";
 import Classmates from "./pages/Classmates";
 import Graduated from "./pages/Graduated";
 import { ThemeProvider } from "@/components/theme-provider";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/layout/navbar";
+import { DataProvider } from "@/components/data-context";
+import Footer from "./components/layout/footer";
 
 function App() {
   return (
-    <>
-      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+      <DataProvider>
         <Navbar />
         <Routes>
           <Route path="/" element={<Classmates />} />
           <Route path="graduated" element={<Graduated />} />
         </Routes>
-      </ThemeProvider>
-    </>
+        <Footer />
+      </DataProvider>
+    </ThemeProvider>
   );
 }
 
