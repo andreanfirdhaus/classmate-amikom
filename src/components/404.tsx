208857b962ec63b2dcdd715a00125fd8f75b45eb
andreanfirdhaus <andreanfirdhaus22@gmail.com>
2024-06-16 23:13:19 +0700

    feat: pagenotfound

---
+++ src/components/404.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function PageNotFound() {
+    return (
+        <>
+            <div className="container mx-auto px-4 max-w-[48.5rem] h-screen flex flex-col justify-center items-center text-center gap-8">
+                <h1 className='text-3xl font-semibold'>Oops!</h1>
+                <p className='text-base'>This shit isn't here, broh. Get the f**k outta here!</p>
+            </div>
+        </>
+    );
+}
