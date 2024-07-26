008f168ac4c73d00751d593335e513efb1608591
andreanfirdhaus <andreanfirdhaus22@gmail.com>
2024-07-26 20:31:14 +0700

    remove: image amikom.png

--- src/assets/assets.ts
+++ src/assets/assets.ts
@@ -1,14 +1,12 @@
 import amikomLight from "./amikom-light.png";
 import amikomDark from "./amikom-dark.png";
-import amikom from "./amikom.png";
 import ImageNotFound from "./404.png";
 
 const assets = {
   amikomLight,
   amikomDark,
-  amikom,
   ImageNotFound,
 };
 
-export { amikomLight, amikomDark, amikom, ImageNotFound };
+export { amikomLight, amikomDark, ImageNotFound };
 export default assets;
