0bd3d92e184349145101562176f29bc93c48e4aa
andreanfirdhaus <andreanfirdhaus22@gmail.com>
2024-07-27 09:41:49 +0700

    style: improve className style /ui

--- src/components/ui/form.tsx
+++ src/components/ui/form.tsx
@@ -93,7 +93,7 @@ const FormLabel = React.forwardRef<
   return (
     <Label
       ref={ref}
-      className={cn(error && "text-destructive", className)}
+      className={cn(error && "", className)}
       htmlFor={formItemId}
       {...props}
     />
@@ -134,7 +134,7 @@ const FormDescription = React.forwardRef<
     <p
       ref={ref}
       id={formDescriptionId}
-      className={cn("text-sm text-muted-foreground", className)}
+      className={cn("text-sm text-muted-foreground ", className)}
       {...props}
     />
   );
