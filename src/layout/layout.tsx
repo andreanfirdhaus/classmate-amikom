90e5a227f91555c4ed5802b6ab394eaf8f9813c6
andreanfirdhaus <andreanfirdhaus22@gmail.com>
2024-10-02 22:41:27 +0700

    fix: path for navbar

--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "@/layout/navbar/navbar";
+import Navbar from "@/layout/navbar";
 import Footer from "@/layout/footer";
 
 export default function Layout() {
