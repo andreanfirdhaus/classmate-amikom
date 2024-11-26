cb2639338cc08e79a5d513f866fee57ec0d99eac
andreanfirdhaus <andreanfirdhaus22@gmail.com>
2024-11-26 14:10:37 +0700

    fixed: active color

--- src/components/sheet-navbar.tsx
+++ src/components/sheet-navbar.tsx
@@ -18,19 +18,19 @@ export default function SheetNavbar() {
       <SheetContent>
         <div className="flex flex-col h-screen gap-6 justify-center items-center">
           <NavLink to="/" className={({ isActive }) =>
-            isActive ? "text-blue-400" : ""
+            isActive ? "font-bold" : "text-foreground"
           }>
-            <p className="text-base font-semibold capitalize tracking-normal">classmate</p>
+            <p className="text-base capitalize tracking-normal">classmate</p>
           </NavLink>
           <NavLink to="/graduated" className={({ isActive }) =>
-            isActive ? "text-blue-400" : ""
+            isActive ? "font-bold" : "text-foreground"
           }>
-            <p className="text-base font-semibold capitalize tracking-normal">
+            <p className="text-base capitalize tracking-normal">
               graduated
             </p>
           </NavLink>
           <Link to="https://github.com/andreanfirdhaus/classmate-amikom" target='_blank' rel='noopener noreferrer'>
-            <p className="text-base font-semibold capitalize tracking-normal">
+            <p className="text-base capitalize tracking-normal">
               repository
             </p>
           </Link>
